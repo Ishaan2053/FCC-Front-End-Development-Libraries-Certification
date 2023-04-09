e73dfc732ec2e561019fcdf8e478ec3959e3634d
Ishaan <ishaan2053@gmail.com>
2023-04-09 16:17:36 +0530

    updated pomodoro clock

--- Projects/pomodoro.js
+++ Projects/pomodoro.js
@@ -1,5 +1,5 @@
 const pomodoroTimer = document.querySelector('#pomodoro-timer');
-
+let type = 'Work'
 const startButton = document.querySelector('#pomodoro-start');
 const pauseButton = document.querySelector('#pomodoro-pause');
 const stopButton = document.querySelector('#pomodoro-stop');
@@ -34,10 +34,65 @@ const toggleClock = (reset) => {
     } else {
       if (isClockRunning === true) {
         // PAUSE THE TIMER
+        clearInterval(clockTimer);
         isClockRunning = false;
       } else {
         // START THE TIMER
         isClockRunning = true;
+
+        clockTimer = setInterval(() => {
+            // decrease time left / increase time spent
+            stepDown()
+        }, 1000)
+        
       }
     }
+  }
+
+
+  const displayCurrentTimeLeftInSession = () => {
+    const secondsLeft = currentTimeLeftInSession
+    let result = ''
+    const seconds = secondsLeft % 60
+    const minutes = parseInt(secondsLeft / 60) % 60
+    let hours = parseInt(secondsLeft / 3600)
+    // add leading zeroes if it's less than 10
+    function addLeadingZeroes(time) {
+      return time < 10 ? `0${time}` : time
+    }
+    if (hours > 0) result += `${hours}:`
+    result += `${addLeadingZeroes(minutes)}:${addLeadingZeroes(seconds)}`
+    pomodoroTimer.innerText = result.toString()
+  }
+
+  pomodoroTimer.innerText = result
+
+  const stopClock = () => {
+    // 1) reset the timer we set
+    clearInterval(clockTimer)
+    // 2) update our variable to know that the timer is stopped
+    isClockRunning = false
+    // reset the time left in the session to its original state
+    currentTimeLeftInSession = workSessionDuration
+    // update the timer displayed
+    displayCurrentTimeLeftInSession()
+  }
+
+  const stepDown = () => {
+    if (currentTimeLeftInSession > 0) {
+      // decrease time left / increase time spent
+      currentTimeLeftInSession--
+    } else if (currentTimeLeftInSession === 0) {
+      // Timer is over -> if work switch to break, viceversa
+      if (type === 'Work') {
+        currentTimeLeftInSession = breakSessionDuration
+        displaySessionLog('Work')
+        type = 'Break'
+      } else {
+        currentTimeLeftInSession = workSessionDuration
+        type = 'Work'
+        displaySessionLog('Break')
+      }
+    }
+    displayCurrentTimeLeftInSession()
   }
