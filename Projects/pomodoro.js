7af37157e3de8572c242a3fde27c2880dee20f93
Ishaan <ishaan2053@gmail.com>
2023-04-09 16:08:20 +0530

    updated pomodoro.js

--- Projects/pomodoro.js
+++ Projects/pomodoro.js
@@ -18,3 +18,26 @@ pauseButton.addEventListener('click', () => {
 stopButton.addEventListener('click', () => {
   toggleClock(true);
 })
+
+let isClockRunning = false;
+
+// in seconds = 25 mins
+let workSessionDuration = 1500;
+let currentTimeLeftInSession = 1500;
+
+// in seconds = 5 mins;
+let breakSessionDuration = 300;
+
+const toggleClock = (reset) => {
+    if (reset) {
+      // STOP THE TIMER
+    } else {
+      if (isClockRunning === true) {
+        // PAUSE THE TIMER
+        isClockRunning = false;
+      } else {
+        // START THE TIMER
+        isClockRunning = true;
+      }
+    }
+  }
